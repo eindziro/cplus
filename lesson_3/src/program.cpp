b6e75cb9b28c7986d54f3929c480049db45acb53
eindziro <eindziro@yandex.ru>
2018-10-09 08:39:39 +0300

    сделал первое задание

---
+++ lesson_3/src/program.cpp
@@ -0,0 +1,14 @@
+#include "program.h"
+
+
+using namespace std;
+
+int main(int argc, char** args)
+{
+	int a = 2;
+	int b = 3;
+	int c = _geek::Mult5(a);
+	cout << "c = "<< c<< endl;
+	return 0;
+	
+}
